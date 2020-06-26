4f36c5e16ebf8b92aeac3a4496ed3b473871f0aa
michaltev <michaltev@gmail.com>
2020-06-26 23:17:18 +0300

    text correction

--- services/status.ts
+++ services/status.ts
@@ -1,6 +1,6 @@
 export class Statuses {
     public static get WAITING():string { return "waiting for the second player to join"; }
-    public static get PLAYING():string { return "plying a game"; }  
+    public static get PLAYING():string { return "playing a game"; }  
     public static get TIE():string { return "the game ended with a tie"; }
     public static get X_WON():string { return "X won the last game"; }  
     public static get O_WON():string { return "O won the last game"; }   
