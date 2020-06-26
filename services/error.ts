a1b32a73df1cf4b30f7e948e624ee22dd6be289c
michaltev <michaltev@gmail.com>
2020-06-27 00:06:19 +0300

    errors messages

---
+++ services/error.ts
@@ -0,0 +1,5 @@
+export class Errors {
+    public static get NOT_THE_TURN():string { return "This is not your turn!"; }
+    public static get NOT_VALID_USER():string { return "Send a valid user"; } 
+    public static get NOT_VALID_CELL():string { return "Choose other cell, this cell is not valid"; } 
+}
