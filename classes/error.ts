b235af3fa97137eb9450ce2cc26334d0bd230963
michaltev <michaltev@gmail.com>
2020-06-27 15:08:43 +0300

    Update error.ts

--- classes/error.ts
+++ classes/error.ts
@@ -1,5 +1,5 @@
 export class Errors {
-    public static get NOT_THE_TURN():string { return "This is not your turn!"; }
+    public static get NOT_THE_TURN():string { return "This is not your turn to play"; }
     public static get NOT_VALID_USER():string { return "Send a valid user"; } 
     public static get NOT_VALID_CELL():string { return "Choose other cell, this cell is not valid"; } 
 }
